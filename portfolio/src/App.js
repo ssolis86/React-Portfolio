ee4d76d577aa18a7cfc9aaf3ab360ef67562341e
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-14 22:26:04 -0500

    setup app bar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <AppBar color="transparent" position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <div>
+            <span>Stephen Solis</span>
+          </div>
+        </Toolbar>
+      </AppBar>
+   </div>
   );
 }
 
