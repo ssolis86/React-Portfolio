e619059bc6bc725dc1f1ac02a4146cf30ae75253
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-15 11:27:57 -0500

    display drawer with relative pages listed

--- portfolio/src/components/Header/index.jsx
+++ portfolio/src/components/Header/index.jsx
@@ -11,9 +11,13 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-
-
-
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import FolderIcon from '@material-ui/icons/Folder';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
 
 const drawerWidth = 240;
 
@@ -122,7 +126,14 @@ return (
           </IconButton>
         </div>
         <Divider />
-
+        <List>
+          {['Home', 'Portfolio', 'Contact'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <HomeIcon /> : <FolderIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
   </div>  
 )
