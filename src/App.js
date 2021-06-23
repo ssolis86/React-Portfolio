c0624c862b204257f0c43ae46933c24e9759a742
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-23 11:09:20 -0500

    github debug

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Contact from './components/Contact';
-import {  BrowserRouter as Router,  Switch,  Route,  Link} from "react-router-dom";
+import {  BrowserRouter as Router,  Switch,  Route} from "react-router-dom";
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles((theme) => ({
-  page: 
-  {
+  root:{
     display: 'flex',
     flexDirection: 'column',
-    height: '100vh'
-  }
-}))
+    minHeight: '100vh',
+    backgroundImage:`url(${process.env.PUBLIC_URL + './src/assets/background_image.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
 function App() {
   const classes = useStyles();
   return (
@@ -25,21 +27,21 @@ function App() {
         <div>
           <Switch>
             <Route path="/contact">
-              <div className={classes.page}>
+              <div className={classes.root}>
                 <Header />
                 <Contact />
                 <Footer />
               </div>
             </Route>
             <Route path="/portfolio">
-            <div className={classes.page}>
+            <div className={classes.root}>
               <Header />
               <Portfolio />
               <Footer />
               </div>
             </Route>
             <Route path="/">
-            <div className={classes.page}>
+            <div className={classes.root}>
               <Header />
               <Home />
               <Footer />
